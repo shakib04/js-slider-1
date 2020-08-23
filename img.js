09ac5a332664691472f6703c755643cd83fcaa4a
shakib04 <51458806+shakib04@users.noreply.github.com>
2020-08-24 00:42:30 +0600

    Create img.js

---
+++ img.js
@@ -0,0 +1,16 @@
+var img1 = "https://scontent.fdac4-1.fna.fbcdn.net/v/t1.0-9/s960x960/118143727_3889913154388451_1449636349208288763_o.jpg?_nc_cat=1&_nc_sid=730e14&_nc_eui2=AeEC6lkLljedusGdUghJO8azdkdQBkvirSF2R1AGS-KtISGSa_4Ir-Kjnxv2703Elv9SBLRi3DCI4HT3G1tyfZk7&_nc_ohc=QIarrsS0MMkAX9NpFsH&_nc_pt=1&_nc_ht=scontent.fdac4-1.fna&_nc_tp=7&oh=20ac5d6430342650fa65ba41eddfb22d&oe=5F6741EA";
+var img2 = "https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80";
+
+var img3 = "https://images.unsplash.com/photo-1569852314545-d5a920f8e043?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80";
+var img4 = "https://images.unsplash.com/photo-1562938249-dea7ace0519b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1225&q=80";
+
+var i5 = "https://images.unsplash.com/photo-1563991457971-816dfe744394?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80";
+
+var i6 = "https://images.pexels.com/photos/5074833/pexels-photo-5074833.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
+
+var i7 = "https://images.pexels.com/photos/4010464/pexels-photo-4010464.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
+var i8 = "https://scontent.fdac4-1.fna.fbcdn.net/v/t1.0-9/p843x403/118349379_10158977894653598_5009197319011287775_o.jpg?_nc_cat=1&_nc_sid=730e14&_nc_eui2=AeFQNZKCIdNWwSLOrukrSkPUQwRen8UuhS1DBF6fxS6FLScceWAvYg-UWZTWu0hXwZ52SqqBIUyhNvgJ8Q9V27j_&_nc_ohc=4NY6PzJYDs4AX8fo28O&_nc_pt=1&_nc_ht=scontent.fdac4-1.fna&_nc_tp=6&oh=eb66506c9f14871ad5a2e9a3f6c3b38c&oe=5F677565";
+var i9 = "https://scontent.fdac4-1.fna.fbcdn.net/v/t1.0-9/p843x403/118155330_3890392131007220_5371651287135390044_o.jpg?_nc_cat=1&_nc_sid=730e14&_nc_eui2=AeHyLd1NBA3WbmD9m-tsDu7ouI94zNboi5S4j3jM1uiLlP7af8FkUaD0hSFQNyqghdHVvmS-KlqVEu0g0Lptot50&_nc_ohc=PuWED2Beb8AAX-szZqB&_nc_pt=1&_nc_ht=scontent.fdac4-1.fna&_nc_tp=6&oh=50a48ce0be872602d49e6126ccb71245&oe=5F6726D0";
+
+var i10 = "https://scontent.fdac4-1.fna.fbcdn.net/v/t1.0-0/p600x600/118229600_3890978497615250_2588967421941511936_o.jpg?_nc_cat=1&_nc_sid=730e14&_nc_eui2=AeEqUWNDPxO-WJMqsONqzaUP8ZlBITzxN1rxmUEhPPE3WvtFz9filBm5Cl2XAA9glS7FGicMsSEDrAYN7pnN-vG0&_nc_ohc=lWrSSvAA7AgAX_ULQO2&_nc_pt=1&_nc_ht=scontent.fdac4-1.fna&_nc_tp=6&oh=3c24db3494bde5bcf078621251543408&oe=5F6894FB";
+
